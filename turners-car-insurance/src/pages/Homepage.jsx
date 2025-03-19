84e8417c0d2701aad25c2c485b723c65fefc849b
sherelynn <sherelynnb@missionreadyhq.com>
2025-03-19 14:26:33 +1300

    Add Homepage component integrating file upload and car identifier
    - Connect UploadPhoto and CarIdentifier components
    -Manage file state between components using prop callbacks

--- turners-car-insurance/src/pages/Homepage.jsx
+++ turners-car-insurance/src/pages/Homepage.jsx
@@ -1,10 +1,27 @@
+import React, { useState } from "react"
 import UploadPhoto from "../components/UploadPhoto"
+import CarIdentifier from "../components/CarIdentifier"
 
 const Homepage = () => {
+  const [uploadedCarFile, setUploadedCarFile] = useState(null)
+
+  const handleFileUploaded = file => {
+    setUploadedCarFile(file)
+  }
+
   return (
-    <div>
-      <UploadPhoto />
+    <div style={{ maxWidth: "800px", margin: "0 auto", padding: "20px" }}>
+      <h1>Turners Car Insurance</h1>
+
+      <UploadPhoto onFileUpload={handleFileUploaded} />
+
+      {uploadedCarFile && (
+        <div style={{ marginTop: "30px" }}>
+          <CarIdentifier file={uploadedCarFile} />
+        </div>
+      )}
     </div>
   )
 }
+
 export default Homepage
